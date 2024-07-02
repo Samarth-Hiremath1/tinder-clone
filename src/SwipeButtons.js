b855ca92015ad7f6722d9610bef11ca533c85518
Samarth Hiremath <samhiremath@ucdavis.edu>
2024-07-02 13:02:16 -0700

    Swipe buttons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './SwipeButtons.css'
 
 import ReplayIcon from '@mui/icons-material/Replay';
 import CloseIcon from '@mui/icons-material/Close';
@@ -7,11 +8,27 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import FlashOnIcon from '@mui/icons-material/FlashOn';
 
 import IconButton from '@mui/material/IconButton';
+import { Icon } from '@mui/material';
 
 function SwipeButtons() {
   return (
     <div className="swipeButtons">
-        
+        <IconButton className='swipeButtons_repeat'>
+            <ReplayIcon fontSize='large' />
+        </IconButton>
+        <IconButton className='swipeButtons_left'>
+            <CloseIcon fontSize='large' />
+        </IconButton>
+        <IconButton className='swipeButtons_star'>
+            <StarRateIcon fontSize='large' />
+        </IconButton>
+        <IconButton className='swipeButtons_right'>
+            <FavoriteIcon fontSize='large' />
+        </IconButton>
+        <IconButton className='swipeButtons_lightning'>
+            <FlashOnIcon fontSize='large' />
+        </IconButton>
+
     </div>
   )
 }
